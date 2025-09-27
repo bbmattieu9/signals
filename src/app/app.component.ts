28d51e759d3a8990fff12333e64159538ecd3b7e
BbMattieu9 <bbmattieu9@gmail.com>
2025-09-27 23:57:18 +0100

    [Initial Commit]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,10 +7,24 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <h1>Welcome to {{title}}!</h1>
 
+    <div>
+      <button (click)="increment()">Increment Counter</button>
+    </div>
+
     <router-outlet />
   `,
   styles: [],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'signalEchad';
+
+  ngOnInit(): void {
+    
+  }
+
+  count = 0;
+
+  increment = () => {
+    this.count = this.count + 1;
+  }
 }
