40440ba72c32d54bc68d4f7ac9f758d9ca29dc90
BbMattieu9 <bbmattieu9@gmail.com>
2025-10-05 23:38:16 +0100

    [Fixes] change folder structure

---
+++ src/app/pages/array-object-signals.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-array-object-signals',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      array-object-signals works!
+    </p>
+  `,
+  styles: ``
+})
+export class ArrayObjectSignalsComponent implements OnInit{
+
+  
+
+
+  ngOnInit(): void { } 
+
+
+}
